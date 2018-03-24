a90549bca6517582e9e13a66122b7bfb5573af9a
akngs <jania902@gmail.com>
2018-03-25 01:36:40 +0900

    .

--- Dockerfile
+++ Dockerfile
@@ -1,78 +1,86 @@
 FROM debian:sid
 LABEL maintainer="jania902@gmail.com"
 
-RUN set -x; \
-    apt-get update \
-    && apt-get install -y --no-install-recommends \
-        apache2 \
-        ca-certificates \
-        certbot \
-        curl \
-        git \
-        imagemagick \
-        libapache2-mod-php7.1 \
-        netcat \
-        php7.1-mysql \
-        php7.1-cli \
-        php7.1-gd \
-        php7.1-curl \
-        php7.1-intl \
-        php7.1-mbstring \
-        php7.1-xml \
-        unzip \
-        zip \
-    && rm -rf /var/lib/apt/lists/* \
-    && rm -rf /var/cache/apt/archives/* \
-    && a2dismod auth_basic -f \
-    && a2dismod autoindex -f \
-    && a2dismod status \
-    && a2enmod rewrite \
-    && a2enmod proxy \
-    && a2enmod proxy_http \
-    && rm /var/www/html/index.html \
-    && php -r "readfile('https://getcomposer.org/installer');" | php \
-    && mv composer.phar /usr/local/bin/composer
+# Install debian packages
+RUN set -x \
+  && apt update \
+  && apt install -y --no-install-recommends \
+    ca-certificates \
+    certbot \
+    curl \
+    git \
+    gettext-base \
+    imagemagick \
+    netcat \
+    nginx \
+    php7.2 \
+    php7.2-apcu \
+    php7.2-cli \
+    php7.2-curl \
+    php7.2-gd \
+    php7.2-fpm \
+    php7.2-intl \
+    php7.2-mbstring \
+    php7.2-mysql \
+    php7.2-xml \
+    python-certbot-nginx \
+    unzip \
+    zip \
+  && rm -rf /var/lib/apt/lists/* \
+  && rm -rf /var/cache/apt/archives/* \
+  && rm /etc/nginx/sites-enabled/default
 
-RUN set -x; \
-    mkdir -p /usr/src \
-    && git clone --depth 1 -b "wmf/1.31.0-wmf.20" https://github.com/wikimedia/mediawiki.git /usr/src/mediawiki \
-    && cd /usr/src/mediawiki \
-    && git submodule update --init skins \
-    && git submodule update --init vendor \
-    && cd extensions \
-    && git submodule update --init --recursive BetaFeatures \
-    && git submodule update --init --recursive CategoryTree \
-    && git submodule update --init --recursive Cite \
-    && git submodule update --init --recursive Citoid \
-    && git submodule update --init --recursive CodeEditor \
-    && git submodule update --init --recursive Echo \
-    && git submodule update --init --recursive Flow \
-    && git submodule update --init --recursive MobileFrontend \
-    && git submodule update --init --recursive PageImages \
-    && git submodule update --init --recursive ParserFunctions \
-    && git submodule update --init --recursive Popups \
-    && git submodule update --init --recursive RevisionSlider \
-    && git submodule update --init --recursive Scribunto \
-    && git submodule update --init --recursive SyntaxHighlight_GeSHi \
-    && git submodule update --init --recursive TemplateData \
-    && git submodule update --init --recursive TextExtracts \
-    && git submodule update --init --recursive VisualEditor \
-    && git submodule update --init --recursive WikiEditor \
-    && git clone https://github.com/wikimedia/mediawiki-extensions-ReplaceText.git ReplaceText \
-    && git clone --recursive https://github.com/jmnote/SimpleMathJax.git \
-    && cd .. \
-    && ( find . -type d -name ".git" && find . -name ".gitignore" && find . -name ".gitmodules" ) | xargs rm -rf
+# Get mediawiki and extensions
+RUN set -x \
+  && mkdir -p /usr/src \
+  && git clone --depth 1 -b "wmf/1.31.0-wmf.26" https://github.com/wikimedia/mediawiki.git /usr/src/mediawiki \
+  && cd /usr/src/mediawiki \
+  && git submodule update --init skins \
+  && git submodule update --init vendor \
+  && cd extensions \
+  && git submodule update --init --recursive BetaFeatures \
+  && git submodule update --init --recursive CategoryTree \
+  && git submodule update --init --recursive Cite \
+  && git submodule update --init --recursive Citoid \
+  && git submodule update --init --recursive CodeEditor \
+  && git submodule update --init --recursive Echo \
+  && git submodule update --init --recursive Flow \
+  && git submodule update --init --recursive MobileFrontend \
+  && git submodule update --init --recursive PageImages \
+  && git submodule update --init --recursive ParserFunctions \
+  && git submodule update --init --recursive Popups \
+  && git submodule update --init --recursive RevisionSlider \
+  && git submodule update --init --recursive Scribunto \
+  && git submodule update --init --recursive SyntaxHighlight_GeSHi \
+  && git submodule update --init --recursive TemplateData \
+  && git submodule update --init --recursive TextExtracts \
+  && git submodule update --init --recursive VisualEditor \
+  && git submodule update --init --recursive WikiEditor \
+  && git clone https://github.com/wikimedia/mediawiki-extensions-ReplaceText.git ReplaceText \
+  && git clone --recursive https://github.com/jmnote/SimpleMathJax.git \
+  && cd .. \
+  && ( find . -type d -name ".git" && find . -name ".gitignore" && find . -name ".gitmodules" ) | xargs rm -rf
 
+# Install and execute composer
 COPY composer.local.json /usr/src/mediawiki/composer.local.json
-RUN set -x; \
-    cd /usr/src/mediawiki \
-    && composer update -o --no-dev \
-    && composer clearcache
+RUN set -x \
+  && php -r "readfile('https://getcomposer.org/installer');" | php \
+  && mv composer.phar /usr/local/bin/composer \
+  && cd /usr/src/mediawiki \
+  && composer update -o --no-dev \
+  && composer clearcache
 
-COPY mediawiki.php.ini /usr/local/etc/php/conf.d/mediawiki.ini
-COPY apache2.conf /etc/apache2/apache2.conf
-COPY mediawiki.apache2.conf /etc/apache2/sites-enabled/mediawiki.conf
-COPY entrypoint.sh /entrypoint.sh
+# Setup nginx
+COPY mediawiki_http.nginx.conf /etc/nginx/sites-available/mediawiki_http.conf
+COPY mediawiki_https.nginx.conf /etc/nginx/sites-available/mediawiki_https.conf
 
+RUN set -x \
+  && rm -rf /var/www/html \
+  && ln -sf /usr/src/mediawiki /var/www/mediawiki
+
+COPY mediawiki.php.ini /etc/php/7.2/fpm/conf.d/mediawiki.ini
+
+# Done
 EXPOSE 80 443
+COPY entrypoint.sh /entrypoint.sh
 CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,118 +1,15 @@
 #!/usr/bin/env bash
-set -e
-
-: ${MEDIAWIKI_SITE_NAME:=MediaWiki}
-: ${MEDIAWIKI_SITE_LANG:=en}
-: ${MEDIAWIKI_ADMIN_USER:=admin}
-: ${MEDIAWIKI_ADMIN_PASS:=rosebud}
-: ${MEDIAWIKI_DB_TYPE:=mysql}
-: ${MEDIAWIKI_DB_SCHEMA:=mediawiki}
-: ${MEDIAWIKI_ENABLE_SSL:=false}
-
-if [ -z "$MEDIAWIKI_DB_HOST" ]; then
-	if [ -n "$MYSQL_PORT_3306_TCP_ADDR" ]; then
-		MEDIAWIKI_DB_HOST=$MYSQL_PORT_3306_TCP_ADDR
-	elif [ -n "$POSTGRES_PORT_5432_TCP_ADDR" ]; then
-		MEDIAWIKI_DB_TYPE=postgres
-		MEDIAWIKI_DB_HOST=$POSTGRES_PORT_5432_TCP_ADDR
-	elif [ -n "$DB_PORT_3306_TCP_ADDR" ]; then
-		MEDIAWIKI_DB_HOST=$DB_PORT_3306_TCP_ADDR
-	elif [ -n "$DB_PORT_5432_TCP_ADDR" ]; then
-		MEDIAWIKI_DB_TYPE=postgres
-		MEDIAWIKI_DB_HOST=$DB_PORT_5432_TCP_ADDR
-	else
-		echo >&2 'error: missing MEDIAWIKI_DB_HOST environment variable'
-		echo >&2 '	Did you forget to --link your database?'
-		exit 1
-	fi
-fi
-
-if [ -z "$MEDIAWIKI_RESTBASE_URL" ]; then
-	export MEDIAWIKI_RESTBASE_URL=restbase-is-not-specified
-fi
-
-if [ -z "$MEDIAWIKI_DB_USER" ]; then
-	if [ "$MEDIAWIKI_DB_TYPE" = "mysql" ]; then
-		echo >&2 'info: missing MEDIAWIKI_DB_USER environment variable, defaulting to "root"'
-		MEDIAWIKI_DB_USER=root
-	elif [ "$MEDIAWIKI_DB_TYPE" = "postgres" ]; then
-		echo >&2 'info: missing MEDIAWIKI_DB_USER environment variable, defaulting to "postgres"'
-		MEDIAWIKI_DB_USER=postgres
-	else
-		echo >&2 'error: missing required MEDIAWIKI_DB_USER environment variable'
-		exit 1
-	fi
-fi
-
-if [ -z "$MEDIAWIKI_DB_PASSWORD" ]; then
-	if [ -n "$MYSQL_ENV_MYSQL_ROOT_PASSWORD" ]; then
-		MEDIAWIKI_DB_PASSWORD=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
-	elif [ -n "$POSTGRES_ENV_POSTGRES_PASSWORD" ]; then
-		MEDIAWIKI_DB_PASSWORD=$POSTGRES_ENV_POSTGRES_PASSWORD
-	elif [ -n "$DB_ENV_MYSQL_ROOT_PASSWORD" ]; then
-		MEDIAWIKI_DB_PASSWORD=$DB_ENV_MYSQL_ROOT_PASSWORD
-	elif [ -n "$DB_ENV_POSTGRES_PASSWORD" ]; then
-		MEDIAWIKI_DB_PASSWORD=$DB_ENV_POSTGRES_PASSWORD
-	else
-		echo >&2 'error: missing required MEDIAWIKI_DB_PASSWORD environment variable'
-		echo >&2 '	Did you forget to -e MEDIAWIKI_DB_PASSWORD=... ?'
-		echo >&2
-		echo >&2 '	(Also of interest might be MEDIAWIKI_DB_USER and MEDIAWIKI_DB_NAME)'
-		exit 1
-	fi
-fi
-
-: ${MEDIAWIKI_DB_NAME:=mediawiki}
-
-if [ -z "$MEDIAWIKI_DB_PORT" ]; then
-	if [ -n "$MYSQL_PORT_3306_TCP_PORT" ]; then
-		MEDIAWIKI_DB_PORT=$MYSQL_PORT_3306_TCP_PORT
-	elif [ -n "$POSTGRES_PORT_5432_TCP_PORT" ]; then
-		MEDIAWIKI_DB_PORT=$POSTGRES_PORT_5432_TCP_PORT
-	elif [ -n "$DB_PORT_3306_TCP_PORT" ]; then
-		MEDIAWIKI_DB_PORT=$DB_PORT_3306_TCP_PORT
-	elif [ -n "$DB_PORT_5432_TCP_PORT" ]; then
-		MEDIAWIKI_DB_PORT=$DB_PORT_5432_TCP_PORT
-	elif [ "$MEDIAWIKI_DB_TYPE" = "mysql" ]; then
-		MEDIAWIKI_DB_PORT="3306"
-	elif [ "$MEDIAWIKI_DB_TYPE" = "postgres" ]; then
-		MEDIAWIKI_DB_PORT="5432"
-	fi
-fi
-
 # Wait for the DB to come up
-echo "Waiting for database to come up at $MEDIAWIKI_DB_HOST:$MEDIAWIKI_DB_PORT..."
-while [ `/bin/nc -w 1 $MEDIAWIKI_DB_HOST $MEDIAWIKI_DB_PORT < /dev/null > /dev/null; echo $?` != 0 ]; do
+echo "Waiting for database to come up at mysql:3306..."
+while [ `/bin/nc -w 1 mysql 3306 < /dev/null > /dev/null 2>&1; echo $?` != 0 ]; do
     sleep 1
 done
 
-export MEDIAWIKI_DB_TYPE MEDIAWIKI_DB_HOST MEDIAWIKI_DB_USER MEDIAWIKI_DB_PASSWORD MEDIAWIKI_DB_NAME
-
-TERM=dumb php -- <<'EOPHP'
-<?php
-// database might not exist, so let's try creating it (just to be safe)
-if ($_ENV['MEDIAWIKI_DB_TYPE'] == 'mysql') {
-	$mysql = new mysqli($_ENV['MEDIAWIKI_DB_HOST'], $_ENV['MEDIAWIKI_DB_USER'], $_ENV['MEDIAWIKI_DB_PASSWORD'], '', (int) $_ENV['MEDIAWIKI_DB_PORT']);
-	if ($mysql->connect_error) {
-		file_put_contents('php://stderr', 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
-		exit(1);
-	}
-	if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($_ENV['MEDIAWIKI_DB_NAME']) . '`')) {
-		file_put_contents('php://stderr', 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
-		$mysql->close();
-		exit(1);
-	}
-	$mysql->close();
-}
-EOPHP
-
-rm -rf /var/www/html
-ln -sf /usr/src/mediawiki /var/www/html
-cd /var/www/html
+cd /var/www/mediawiki
 
 : ${MEDIAWIKI_CUSTOM:=/custom}
 rm -rf LocalSettings.post.php
-ln -s "$MEDIAWIKI_CUSTOM/LocalSettings.post.php" LocalSettings.post.php
+envsubst '${MEDIAWIKI_PROTOCOL} ${MEDIAWIKI_SITE_SERVER}' < ${MEDIAWIKI_CUSTOM}/LocalSettings.post.php > LocalSettings.post.php
 rm -rf statics
 ln -s "$MEDIAWIKI_CUSTOM/statics" statics
 
@@ -129,25 +26,34 @@ if [ -d "$MEDIAWIKI_SHARED" ]; then
 	fi
 fi
 
-if [ $MEDIAWIKI_ENABLE_SSL = true ]; then
+# SSL
+rm -f /etc/nginx/sites-enabled/*
+if [ "$MEDIAWIKI_PROTOCOL" == "https" ]; then
     if [ -d "$MEDIAWIKI_SHARED/letsencrypt" ]; then
         cp -r "$MEDIAWIKI_SHARED/letsencrypt" /etc
     else
         certbot certonly --standalone --email "${MEDIAWIKI_ADMIN_EMAIL}" --agree-tos -d "${MEDIAWIKI_SITE_SERVER}" -n
         cp -r /etc/letsencrypt "$MEDIAWIKI_SHARED"
+
+        openssl dhparam 2048 > $MEDIAWIKI_SHARED/dhparam.pem
     fi
-    a2enmod ssl
+    cp $MEDIAWIKI_SHARED/dhparam.pem /etc/nginx/dhparam.pem
+    envsubst '${MEDIAWIKI_SITE_SERVER}' < /etc/nginx/sites-available/mediawiki_https.conf > /etc/nginx/sites-enabled/mediawiki_https
+else
+    envsubst '${MEDIAWIKI_SITE_SERVER}' < /etc/nginx/sites-available/mediawiki_http.conf > /etc/nginx/sites-enabled/mediawiki_http
 fi
+
 
 # If there is no LocalSettings.php, create one using maintenance/install.php
 if [ ! -e "LocalSettings.php" ]; then
+    echo >&2 'info: Running maintenance/install.php'
 	php maintenance/install.php \
-		--confpath /var/www/html \
-		--dbname "$MEDIAWIKI_DB_NAME" \
-		--dbschema "$MEDIAWIKI_DB_SCHEMA" \
-		--dbport "$MEDIAWIKI_DB_PORT" \
-		--dbserver "$MEDIAWIKI_DB_HOST" \
-		--dbtype "$MEDIAWIKI_DB_TYPE" \
+		--confpath /var/www/mediawiki \
+		--dbname "mw" \
+		--dbschema "mw" \
+		--dbport "3306" \
+		--dbserver "mysql" \
+		--dbtype "mysql" \
 		--dbuser "$MEDIAWIKI_DB_USER" \
 		--dbpass "$MEDIAWIKI_DB_PASSWORD" \
 		--installdbuser "$MEDIAWIKI_DB_USER" \
@@ -175,15 +81,17 @@ if [ ! -e "LocalSettings.php" ]; then
         mv images "$MEDIAWIKI_SHARED/images"
         ln -s "$MEDIAWIKI_SHARED/images" images
     fi
+    echo >&2 'info: Finished running maintenance/install.php'
 fi
 
 # Update
-echo >&2 'info: Running maintenance/update.php';
-MW_INSTALL_PATH=/var/www/html php maintenance/update.php --quick --conf ./LocalSettings.php
+echo >&2 'info: Running maintenance/update.php'
+MW_INSTALL_PATH=/var/www/mediawiki php maintenance/update.php --quick --conf ./LocalSettings.php
+echo >&2 'info: Finished running maintenance/update.php'
 
-# Fix file ownership and permissions
-chown -R www-data: /usr/src/mediawiki
+# Run
 chown -R www-data: /data/images
 
-# Done
-apachectl -e info -D FOREGROUND
+echo >&2 'info: Starting PHP FPM and Nginx...'
+service php7.2-fpm start
+nginx -g "daemon off;"
